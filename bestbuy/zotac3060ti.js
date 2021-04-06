b1979479b5ff474846ed4f25bebd7679bdb6d6cb
Deku Tran <dekutran@gmail.com>
2021-04-06 15:04:10 -0700

    ocd

--- bestbuy/zotac3060ti.js
+++ bestbuy/zotac3060ti.js
@@ -26,7 +26,7 @@ const fs = require('fs');
             try {
                 await page.goto('https://www.bestbuy.ca/en-ca/collection/rtx-30-series-graphic-cards/316108?path=category%253AComputers%2B%2526%2BTablets%253Bcategory%253APC%2BComponents%253Bcategory%253AGraphics%2BCards%253Bcustom0graphicscardmodel%253AGeForce%2BRTX%2B3060%2BTi', { timeout: 30000 });
                 await page.click('text=ZOTAC GeForce RTX 3060 Ti Twin Edge OC 8GB GDDR6 Video Card', { timeout: 30000 });
-                let response = await page.waitForNavigation()
+                let response = await page.waitForNavigation();
                 if (response.status() !== 404) {
                     isPageLoaded = true;
                 }
